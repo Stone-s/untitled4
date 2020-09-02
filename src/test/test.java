a342d7909a3bc777105bba51c5566fe0ebaed8f6
Stone <sly990411@outlook.com>
2020-09-02 18:24:19 +0800

    冲突测试

--- src/test/test.java
+++ src/test/test.java
@@ -1,11 +1,10 @@
 package test;
 
-import javax.jws.soap.SOAPBinding;
 import java.util.Random;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("这是开发分支上的操作");
+        System.out.println("这是开发分支。我要给你删了");
         System.out.println("这是第二次提交");
     }
 }
