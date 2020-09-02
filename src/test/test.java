4fcddc930d7b8c3c9407e7a917fa79e613b90c5f
Stone <sly990411@outlook.com>
2020-09-02 17:45:06 +0800

    这是第一次提交哦

--- src/test/test.java
+++ src/test/test.java
@@ -3,6 +3,6 @@ package test;
 public class test {
     public static void main(String[] args) {
         System.out.println("我靠了，这是什么情况啊");
-        asfasdfasdfadsfd
+        System.out.println("这是第二次提交");
     }
 }
