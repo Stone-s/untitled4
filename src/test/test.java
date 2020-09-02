412bdaece2b28cf5a277bea2018f600758ab90d2
Stone <sly990411@outlook.com>
2020-09-02 18:30:59 +0800

    冲突测试

--- src/test/test.java
+++ src/test/test.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class test {
     public static void main(String[] args) {
         System.out.println("121212这是开发分支上的操作");
+
         System.out.println("这是第二次提交");
     }
 }
