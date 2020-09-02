f0f3577062df06c9ccc9c07ba9c06f6937d63c91
Stone <sly990411@outlook.com>
2020-09-02 17:48:17 +0800

    冲突测试

--- src/test/test.java
+++ src/test/test.java
@@ -1,7 +1,10 @@
 package test;
 
+import java.util.Random;
+
 public class test {
     public static void main(String[] args) {
+        Random random = new Random();
         System.out.println("我靠了，这是什么情况啊");
         System.out.println("这是第二次提交");
     }
